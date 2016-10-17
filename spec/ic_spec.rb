21ec5f47b738f4aeed86aba8ce666468b7392beb
SimplyAMan <simplyaman@gmail.com>
2016-10-17 17:01:05 +0300

    IC test cases have translated in ukrainian

--- spec/ic_spec.rb
+++ spec/ic_spec.rb
@@ -4,23 +4,23 @@ require 'ruby-plsql'
 
 describe 'IsCard tools' do
 
-  it 'should return null if IsCard account not in plat purpose' do
+  it 'має повертатись null якщо не вказаний номер рахунку IsCard в призначенні платежу' do
     expect(plsql.pkg_alfa_ic.getaccfromplatpurpose('призначення платежу без рахунка IsCard')).to eq nil
   end
 
-  it 'should return IsCard account number from end of plat purpose' do
+  it 'має повертатись номер рахунку IsCard з кінця призначення платежу' do
     expect(plsql.pkg_alfa_ic.getaccfromplatpurpose('рахунок IsCard 26250123456789')).to eq '26250123456789'
   end
 
-  it 'should return IsCard account number from middle plat purpose' do
+  it 'має повертатись номер рахунку IsCard з середини призначення платежу' do
     expect(plsql.pkg_alfa_ic.getaccfromplatpurpose('рахунок IsCard 26250123456789 в призначенні платежу')).to eq '26250123456789'
   end
 
-  it 'should return IsCard account number from begin plat purpose' do
+  it 'має повертатись номер рахунку IsCard з початку призначення платежу' do
     expect(plsql.pkg_alfa_ic.getaccfromplatpurpose('26250123456789 - рахунок в призначенні платежу')).to eq '26250123456789'
   end
 
-  it 'should return null when not 2625% account in plat purpose' do
+  it 'має повертатись null коли в призначенні платежу вказано номер рахунку не 2625%' do
     expect(plsql.pkg_alfa_ic.getaccfromplatpurpose('26200123456789 - рахунок в призначенні платежу')).to eq nil
   end
 
