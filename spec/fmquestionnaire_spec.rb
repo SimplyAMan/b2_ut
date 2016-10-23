19e91c9128e2c26a055c65146ed1ab938c8b9ee2
SimplyAMan <simplyaman@gmail.com>
2016-10-23 13:20:31 +0300

    Refactor

--- spec/fmquestionnaire_spec.rb
+++ spec/fmquestionnaire_spec.rb
@@ -2,39 +2,57 @@ require 'rspec'
 require 'rubygems'
 require 'ruby-plsql'
 
-plsql.eprlogin_second
-
 describe 'FinMon Questionnaire' do
-  questionnaire = plsql.select(:first,"
-                              SELECT q.*
-                                FROM FMQuestionnaire q
-                               WHERE q.ID IN (SELECT FMQuestionnaireId
-                                                FROM Fmqs_Paramhistory
-                                               WHERE ValueText IS NOT NULL
-                                                 AND AgileParamId = 115847)
-                                 AND q.ID IN (SELECT FMQuestionnaireId
-                                                FROM Fmqs_Paramhistory
-                                               WHERE ValueText IS NOT NULL
-                                                 AND AgileParamId = 115848)")
-
-  contragentid = questionnaire[:contragentid]
-  questionnaireid = questionnaire[:id]
-
-  controller = plsql.select(:first,"
-                                  SELECT ValueText
-                                    FROM Fmqs_ParamHistory
-                                   WHERE FMQuestionnaireId = #{questionnaireid}
-                                     AND AgileParamId = 115847")[:valuetext]
-  accountsinotherbanks = plsql.select(:first,"
-                                  SELECT ValueText
-                                    FROM Fmqs_ParamHistory
-                                   WHERE FMQuestionnaireId = #{questionnaireid}
-                                     AND AgileParamId = 115848")[:valuetext]
-  lfmquestionnaire = plsql.select(:all,"SELECT * FROM TABLE(pkg_AlfaFMQuestionnaire.getContragentByID(pContragentId => #{contragentid}))")
-  it 'перевірка повернення контролера з доп.параметрів' do
-    expect(lfmquestionnaire[0][:controller]).to eq controller
+  before(:all) do
+    plsql.eprlogin_second
   end
-  it 'перевірка повернення рахунків в інших банках з доп.параметрів' do
-      expect(lfmquestionnaire[0][:accountsinotherbanks]).to eq accountsinotherbanks
-  end
+
+  # after save
+  # should exists
+  # should contain saved data
+  # should contain add param
+  context 'доп.параметри опитувальника'
+    before(:all) do
+      questionnaire = plsql.select(:first,"
+                                  SELECT q.*
+                                    FROM FMQuestionnaire q
+                                   WHERE q.ID IN (SELECT FMQuestionnaireId
+                                                    FROM Fmqs_Paramhistory
+                                                   WHERE ValueText IS NOT NULL
+                                                     AND AgileParamId = 115847)
+                                     AND q.ID IN (SELECT FMQuestionnaireId
+                                                    FROM Fmqs_Paramhistory
+                                                   WHERE ValueText IS NOT NULL
+                                                     AND AgileParamId = 115848)")
+
+      @contragentid = questionnaire[:contragentid]
+      @questionnaireid = questionnaire[:id]
+
+      @fmquestionnaire = plsql.select(:all,<<-SQL
+                                          SELECT *
+                                            FROM TABLE(pkg_AlfaFMQuestionnaire.getContragentByID(
+                                                          pContragentId => #{@contragentid}))
+                                          SQL
+                                          )
+    end
+    
+    it 'перевірка повернення контролера з доп.параметрів' do
+      controller = plsql.select(:first,"
+                                      SELECT ValueText
+                                        FROM Fmqs_ParamHistory
+                                       WHERE FMQuestionnaireId = #{@questionnaireid}
+                                         AND AgileParamId = 115847
+                                         ")[:valuetext]
+
+      expect(@fmquestionnaire[0][:controller]).to eq controller
+    end
+    it 'перевірка повернення рахунків в інших банках з доп.параметрів' do
+      accountsinotherbanks = plsql.select(:first,"
+                                      SELECT ValueText
+                                        FROM Fmqs_ParamHistory
+                                       WHERE FMQuestionnaireId = #{@questionnaireid}
+                                         AND AgileParamId = 115848")[:valuetext]
+
+      expect(@fmquestionnaire[0][:accountsinotherbanks]).to eq accountsinotherbanks
+    end
 end
