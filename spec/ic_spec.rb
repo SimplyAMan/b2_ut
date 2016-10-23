7c63749b2010d2cb463968d4fd151d4c12ec15f6
SimplyAMan <simplyaman@gmail.com>
2016-10-23 13:27:28 +0300

    Refactor

--- spec/ic_spec.rb
+++ spec/ic_spec.rb
@@ -1,26 +1,28 @@
+# coding: utf-8
+
 require 'rspec'
 require 'rubygems'
 require 'ruby-plsql'
 
-describe 'IsCard tools' do
+describe 'pkg_Alfa_IC.GetAccFromPlatPurpose' do
 
-  it 'має повертатись null якщо не вказаний номер рахунку IsCard в призначенні платежу' do
+  it 'має повертати null якщо не вказаний номер рахунку IsCard в призначенні платежу' do
     expect(plsql.pkg_alfa_ic.getaccfromplatpurpose('призначення платежу без рахунка IsCard')).to eq nil
   end
 
-  it 'має повертатись номер рахунку IsCard з кінця призначення платежу' do
+  it 'має повертати номер рахунку IsCard з кінця призначення платежу' do
     expect(plsql.pkg_alfa_ic.getaccfromplatpurpose('рахунок IsCard 26250123456789')).to eq '26250123456789'
   end
 
-  it 'має повертатись номер рахунку IsCard з середини призначення платежу' do
+  it 'має повертати номер рахунку IsCard з середини призначення платежу' do
     expect(plsql.pkg_alfa_ic.getaccfromplatpurpose('рахунок IsCard 26250123456789 в призначенні платежу')).to eq '26250123456789'
   end
 
-  it 'має повертатись номер рахунку IsCard з початку призначення платежу' do
+  it 'має повертати номер рахунку IsCard з початку призначення платежу' do
     expect(plsql.pkg_alfa_ic.getaccfromplatpurpose('26250123456789 - рахунок в призначенні платежу')).to eq '26250123456789'
   end
 
-  it 'має повертатись null коли в призначенні платежу вказано номер рахунку не 2625%' do
+  it 'має повертати null коли в призначенні платежу вказано номер рахунку не 2625%' do
     expect(plsql.pkg_alfa_ic.getaccfromplatpurpose('26200123456789 - рахунок в призначенні платежу')).to eq nil
   end
 
